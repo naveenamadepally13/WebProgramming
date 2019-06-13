24e9dcb2dac248bf2cb149f599bf0495ef319673
naveenamadepally13 <47007814+naveenamadepally13@users.noreply.github.com>
2019-06-13 17:03:05 -0500

    Added ICP4

---
+++ ICP4/Todo/todo.js
@@ -0,0 +1,30 @@
+var app = angular.module('myApp', []);
+app.controller('todoCtrl', function($scope) {
+    $scope.todoList = [];
+
+    $scope.todoAdd = function() {
+        $scope.todoList.push({todoText:$scope.todoInput, done:false});
+        console.log($scope.todoList);
+        $scope.todoInput = "";
+    };
+
+    $scope.remove = function(value) {
+        var oldList = $scope.todoList;
+        $scope.todoList = [];
+        angular.forEach(oldList, function(x) {
+            if (value === x.value)
+                $scope.todoList.push(x);
+           // $scope.items.splice(index, 1);
+        });
+        console.log($scope.todoList);
+    };
+    $scope.remaining =function()
+    {
+        var count = 0;
+        angular.forEach($scope.todoList, function(todo) {
+            count += todo.done ? 0 : 1;
+        });
+        return count;
+    }
+
+});
